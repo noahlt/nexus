ad76688dda2336bd20e13a8c21df04bf395ef42b
Eric Liang <forward.eric@gmail.com>
2008-10-13 23:12:21 -0700

    a slightly better idea - like the newspaper cover

--- media/grid.js
+++ media/grid.js
@@ -93,6 +93,8 @@ $(document).ready(function() {
 	});
 
 	$("#tags #alltags").click(function() {
+		$("#dates li").removeClass("activedate");
+		selecting_dates = false;
 		$("#tags li").removeClass("useless");
 		$("#tags .activetag").not("#alltags")
 			.removeClass("activetag")
